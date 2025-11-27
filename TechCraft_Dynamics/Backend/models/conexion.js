22aa4e09725670cad05e8a8a58779c1313db8945
Daniel Miranda <johandanielmirandamoreno@gmail.com>
2025-11-26 21:06:28 -0500

    conexion 2.0

--- TechCraft_Dynamics/Backend/models/conexion.js
+++ TechCraft_Dynamics/Backend/models/conexion.js
@@ -1,17 +1,40 @@
 // src/models/conexion.js
-require('dotenv').config({ path: './.env' });
-const mysql = require('mysql2/promise');
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+import { URL } from "url";
+
+dotenv.config({ path: "./.env" });
 
 const isProd = process.env.NODE_ENV === "production";
 
-const pool = isProd
-  ? mysql.createPool(process.env.MYSQL_URL) // usa Railway
-  : mysql.createPool({                     // usa local
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    });
+let pool;
 
-module.exports = pool;
+if (isProd) {
+  // Parseamos la MYSQL_URL para pasarla como objeto
+  const dbUrl = new URL(process.env.MYSQL_URL);
+
+  pool = mysql.createPool({
+    host: dbUrl.hostname,
+    port: dbUrl.port,
+    user: dbUrl.username,
+    password: dbUrl.password,
+    database: dbUrl.pathname.replace("/", ""),
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
+} else {
+  // Configuración local
+  pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
+}
+
+export default pool;
