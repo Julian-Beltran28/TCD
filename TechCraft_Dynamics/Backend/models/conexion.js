766ed5913e28fd8d6e081e9aa84a2f38db526eb5
Daniel Miranda <johandanielmirandamoreno@gmail.com>
2025-11-26 21:02:21 -0500

    cambios railway base datos

--- TechCraft_Dynamics/Backend/models/conexion.js
+++ TechCraft_Dynamics/Backend/models/conexion.js
@@ -2,10 +2,11 @@
 require('dotenv').config({ path: './.env' });
 const mysql = require('mysql2/promise');
 
-// Usamos la URL si existe
-const pool = process.env.MYSQL_URL
-  ? mysql.createPool(process.env.MYSQL_URL)
-  : mysql.createPool({
+const isProd = process.env.NODE_ENV === "production";
+
+const pool = isProd
+  ? mysql.createPool(process.env.MYSQL_URL) // usa Railway
+  : mysql.createPool({                     // usa local
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
